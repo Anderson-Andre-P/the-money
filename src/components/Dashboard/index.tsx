2b1c60bb8cb159554709db9b6cbf1af071a9c1dd
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2021-12-12 15:33:53 -0300

    feat: componente TransactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Sumary } from "../Sumary";
+import { TransactiosTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
     return (
         <Container>
             <Sumary/>
+            <TransactiosTable />
         </Container>
     ) 
 }
