660befd314f14d7ba1634df1200b261b3c3d1796
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2021-12-12 12:58:19 -0300

    feat: adicionando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+
+
 export function App() {
   return (
     <div className="App">
-      <h1>Olá, mundo!</h1>
+      <GlobalStyle />
+      <h1>
+        Olá, mundo!
+      </h1>
     </div>
   );
 }
