21ae86081ac7d75d3cec8c0ce189c7ff7746bac3
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2021-12-12 17:56:12 -0300

    feat: consumindo API

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'food',
+          creatAt: '21/03/2021'
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
